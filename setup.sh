c3e76d938d258516136c6b9aeac596b98bc69e4c
Marcel Puyat <marcelp@alumni.stanford.edu>
2015-06-06 01:53:36 -0700

    Add instructions for setting up monitor/screen to have no black borders

--- setup.sh
+++ setup.sh
@@ -11,5 +11,13 @@ sudo ln -s /usr/share/zoneinfo/US/Pacific /etc/localtime;
 echo "Installing Chromium";
 sudo apt-get install chromium;
 
+
+
+# The following are instructions that have to be followed (instead of this script actually performing them for you)
+
 # Nodejs
 echo "Install nodejs by following instructions at: http://howtonode.org/how-to-install-nodejs";
+
+# Remove black borders on monitor display
+echo "If monitor doesn't fully fit the screen (i.e. black borders), edit /boot/config.txt:";
+echo "disable_overscan=1 and there should be no overscan_left/right/top/bottom";
