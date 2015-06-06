554757f1ea80c9c0f95306bf870e5fab9ca23d0b
Marcel Puyat <marcelp@alumni.stanford.edu>
2015-06-06 01:00:29 -0700

    Fix node installation message to use correct url

--- setup.sh
+++ setup.sh
@@ -7,10 +7,9 @@ echo "Setting up timezone to Pacific";
 sudo rm /etc/localtime;
 sudo ln -s /usr/share/zoneinfo/US/Pacific /etc/localtime;
 
-# Nodejs
-echo "Install nodejs: Download uncompiled version from: and ://nodejs.org/download/ then follow instructions under Building on Linux here: https://github.com/joyent/node/wiki/Installation"; 
-
 # Chromium
 echo "Installing Chromium";
 sudo apt-get install chromium;
 
+# Nodejs
+echo "Install nodejs by following instructions at: http://howtonode.org/how-to-install-nodejs";
